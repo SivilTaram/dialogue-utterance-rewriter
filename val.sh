7c6a095f0b2c3bdf92924ed52630533d0de92283
Qian <qianlxc@outlook.com>
2020-05-27 08:41:57 +0800

    Update val.sh

--- val.sh
+++ val.sh
@@ -1,11 +1,11 @@
 CUDA_VISIBLE_DEVICES=1 python run_summarization.py\
     --mode=eval \
-    --data_path=../data/val.txt \
-    --vocab_path=../data/vocab.txt \
-    --log_root=./log \
+    --data_path=data/val.txt \
+    --vocab_path=data/vocab.txt \
+    --log_root=log \
     --exp_name=extractive \
     --vocab_size=30000 \
     --coverage=0 \
     --batch_size=128 \
     --convert_to_coverage_model=0 \
-    --restore_best_model=0
+    --restore_best_model=1
